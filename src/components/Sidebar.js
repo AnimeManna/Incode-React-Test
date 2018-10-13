11fa72610d0a8fe29dc429e09c8c0d49186e2715
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:40:53 +0300

    Add Input from semantic-ui-react and use him

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react'
 
+import {
+    Input
+} from 'semantic-ui-react'
+
 class Sidebar extends Component{
     render(){
         return(
             <div>
-                Hello sidebar
+                <Input icon="search" placeholder="Search" width="100%" />
             </div>
         )
     }
