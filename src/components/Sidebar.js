e0de69a9bc5312253376870389697efed45df5f6
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 19:42:25 +0300

    Add eventChange from search input and refractor mapStateToProps

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,7 @@ import {Link} from 'react-router-dom'
 
 import {xhrData} from "../actions/xhrData";
 import {getClient} from "../actions/getClient";
+import {searchClient} from "../actions/searchClient";
 
 import {
     Input,
@@ -16,31 +17,53 @@ import {
 
 class Sidebar extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            searchInput: ''
+        }
+        this.changeSearchInput = this.changeSearchInput.bind(this)
+    }
+
     componentDidMount() {
         this.props.xhrData()
     }
 
+    async changeSearchInput(event) { // сделал асинхронным потому что setState работает позже и первое значение пропадает
+        const {value} = event.target
+        await this.setState({
+            searchInput: value
+        });
+        this.props.searchClient(this.state.searchInput);
+        console.log(this.state)
+
+    }
+
     render() {
-        console.log(this.props.clients);
+        console.log(this.props.viewClients);
         // безуспешные попытки найти как это сделать в semantic-ui а не через style
         return (
             <Segment>
-                <Input icon="search" placeholder="Search"  />
-                <List style={{overflow:'auto', maxHeight:500, minWidth:'100%'}} >
-                    {this.props.clients.map((value) => {
+                <Input icon="search" placeholder="Search" onChange={this.changeSearchInput}/>
+                <List style={{overflow: 'auto', maxHeight: 500, minWidth: '100%'}}>
+                    {this.props.viewClients.map((value) => {
                         const {general} = value;
                         const {job} = value
                         return (
-                            <Link to={`/client/${value.id}`} key={value.id} style={{color:'black',padding:10}}>
-                                    <List.Item style={{display:'flex'}} onClick={()=>{
-                                        this.props.getClient(value.id)
-                                    }} >
-                                        <Image  src={general.avatar} style={{width:80, height:80}} />
-                                        <List.Content>
-                                            <List.Header> {general.firstName} {general.lastName} </List.Header>
-                                            <List.Description> {job.company} </List.Description>
-                                        </List.Content>
-                                    </List.Item>
+                            <Link to={`/client/${value.id}`} key={value.id} style={{color: 'black', padding: 10}}>
+                                <List.Item style={{display: 'flex'}} onClick={() => {
+                                    this.props.getClient(value.id)
+                                }}>
+                                    <Image src={general.avatar} style={{width: 80, height: 80}}/>
+                                    <List.Content>
+                                        <List.Header style={{
+                                            fontSize: 20,
+                                            padding: 5
+                                        }}> {general.firstName} {general.lastName} </List.Header>
+                                        <List.Description
+                                            style={{fontSize: 13, padding: 5}}> {job.title} </List.Description>
+                                    </List.Content>
+                                </List.Item>
                             </Link>
                         )
                     })}
@@ -52,12 +75,13 @@ class Sidebar extends Component {
 
 const mapDispatchToProps = {
     xhrData,
-    getClient
+    getClient,
+    searchClient
 }
 
 export default connect(
     state => ({
-        clients: state.clientsReducer.clients
+        viewClients: state.clientsReducer.viewClients
     }),
     mapDispatchToProps
 )(Sidebar)
