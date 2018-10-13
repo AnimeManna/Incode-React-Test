451c2937e95783ce9b56410ba094cb858ef56b5d
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 14:46:54 +0300

    Creating customer processing

--- src/reducers/clientsReducer.js
+++ src/reducers/clientsReducer.js
@@ -1,5 +1,26 @@
-const initialState=[]
+import {
+    XHR_CLIENTS_SUCCESS,
+} from "../ActionsType/ActionTypes";
+
+const initialState={
+    clients:[
+
+    ]
+}
+
 
 export default (state=initialState, action) =>{
-    return state
+    switch (action.type){
+        case XHR_CLIENTS_SUCCESS:{
+            return{
+                ...state,
+                clients:[
+                    ...action.payload
+                ]
+            }
+        }
+        default:{
+            return state
+        }
+    }
 }
