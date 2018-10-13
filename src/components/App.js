e5d0eebfc2fcc548b7eec3b9a89a135c2a817ed1
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:38:03 +0300

    Add semantic-ui and use for create Grid

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,22 @@ import React,{Component} from 'react'
 import Sidebar from './Sidebar'
 import Layout from '../containers/Layout'
 
+import {
+    Grid
+} from 'semantic-ui-react'
+
 class App extends Component{
     render(){
         return(
             <div>
-                <Sidebar/>
-                <Layout/>
+                <Grid columns={2} divided>
+                    <Grid.Column width={3}>
+                        <Sidebar/>
+                    </Grid.Column>
+                    <Grid.Column width={11}>
+                        <Layout/>
+                    </Grid.Column>
+                </Grid>
             </div>
         )
     }
