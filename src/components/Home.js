593228f7dfa3c383097e2c09d88c467dd3af9a47
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 15:48:20 +0300

    Update home from Incode-Group

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react'
 
+import {
+    Segment
+} from 'semantic-ui-react'
+
 class Home extends Component{
     render(){
         return(
-            <div>
-                Hello Incode Group
-            </div>
+            <Segment textAlign="center" >
+                Welcome Incode-Group sure you will like it)
+            </Segment>
         )
     }
 }
