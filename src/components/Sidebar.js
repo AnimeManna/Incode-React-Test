145ea7c8e0ab5d5d0863fd53593b685e4bec699b
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:49:12 +0300

    Use connect from download clients info

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react'
 
+import {connect} from 'react-redux'
+
+import {xhrData} from "../actions/xhrData";
+
 import {
     Input
 } from 'semantic-ui-react'
 
 class Sidebar extends Component{
+
+    componentDidMount(){
+        this.props.xhrData()
+    }
+
     render(){
+        console.log(this.props.clients);
         return(
             <div>
                 <Input icon="search" placeholder="Search" width="100%" />
@@ -14,4 +24,13 @@ class Sidebar extends Component{
     }
 }
 
-export default Sidebar
+const mapDispatchToProps = {
+    xhrData
+}
+
+export default connect(
+    state => ({
+        clients: state.clientsReducer.clients
+    }),
+    mapDispatchToProps
+)(Sidebar)
