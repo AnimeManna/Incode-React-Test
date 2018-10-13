a1f0664f202b06c81f47b9e67aff64176bf7c898
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:08:06 +0300

    Creating a Home component to display the initial information

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                Hello Incode Group
+            </div>
+        )
+    }
+}
+
+export default Home
