d4bcc25889c43d23116fe9afc87cebd14a53e7b0
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:09:23 +0300

    Adding Route and Switch to correctly display new components by uri. Importing and using new components

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
 
+import {Route, Switch} from 'react-router-dom'
+import Home from '../components/Home'
+import Client from '../components/Client'
+
 class Layout extends Component{
     render(){
         return(
             <div>
-                Hello Layout
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path='/client/:id' component={Client} />
+                </Switch>
             </div>
         )
     }
