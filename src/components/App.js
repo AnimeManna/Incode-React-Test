088b8292b4a0cebd1e8c04755d348b842ff58b04
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 12:49:26 +0300

    importing Sidebar and Layout components and using them

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React,{Component} from 'react'
 
+import Sidebar from './Sidebar'
+import Layout from '../containers/Layout'
+
 class App extends Component{
     render(){
         return(
             <div>
-                Hello Incode-Group
+                <Sidebar/>
+                <Layout/>
             </div>
         )
     }
