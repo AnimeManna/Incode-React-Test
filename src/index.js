a633dcd6e8eb548ab1d82335198386876c60d037
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-13 13:01:01 +0300

    Adds the necessary elements from redux react-redux and their use. Store creation and connection Provider

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+//Подключаем нужные нам библиотеки
 import React from 'react'
 import ReactDOM from 'react-dom'
 
@@ -5,9 +6,19 @@ import App from './components/App'
 
 import {BrowserRouter} from 'react-router-dom'
 
+import rootReducer from './reducers/rootReducer'
+
+import {Provider} from 'react-redux'
+
+import {createStore} from 'redux'
+
+const store = createStore(rootReducer) // создаем store для нашего приложение и передаем в него rootReducer
+
 ReactDOM.render(
     <BrowserRouter>
-            <App />
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </BrowserRouter>
     , document.getElementById('root')
 );
