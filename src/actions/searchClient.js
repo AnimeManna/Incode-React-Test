3d77df7e8524e960ab86ac6c5f85def4cab7f7fc
Gladko Maxim <maxuniwersal@gmail.com>
2018-10-15 02:38:11 +0300

    Delete last console.log in searchClient

--- src/actions/searchClient.js
+++ src/actions/searchClient.js
@@ -1,6 +1,5 @@
 import {SEARCH_CLIENT} from "../actionsType/ActionTypes";
 
 export const searchClient = (search) => dispatch =>{
-    console.log(search)
     dispatch({type:SEARCH_CLIENT,payload:search})
 }
